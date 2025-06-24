9a4b5210d94c67c4e58b73aa7e05bd4c83e08cc6
Abiyyu Umar Thoyyib <abiyyu.umar16@gmail.com>
2025-06-24 23:25:55 +0700

    fix: Update invoice email content to include order details and payment deadline

--- resources/views/emails/invoice-mail.blade.php
+++ resources/views/emails/invoice-mail.blade.php
@@ -1,5 +1,6 @@
 <h1>Halo {{ $nama_customer }},</h1>
 
-<p>Ini adalah invoice Anda untuk bulan {{ \Carbon\Carbon::parse($tanggal)->format('F Y') }}.</p>
+<p>Silakan lihat rincian pesanan Anda melalui Invoice {{ $no_invoice }} terlampir. </p>
 
-<p>Silahkan segera melakukan pembayaran. Terima kasih!</p>
+
+<p>Silahkan segera melakukan pembayaran sebelum jatuh tempo. Terima kasih!</p>
