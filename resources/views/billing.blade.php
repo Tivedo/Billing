2c8f691bf1a401407a1f51bca795a7edf7d20650
Abiyyu Umar Thoyyib <abiyyu.umar16@gmail.com>
2025-07-26 13:07:12 +0700

    fix: Update button styles for download links in billing view

--- resources/views/billing.blade.php
+++ resources/views/billing.blade.php
@@ -225,7 +225,7 @@
                                             Faktur belum tersedia
                                             @elseif ($d['url_faktur'] != null)
                                             <a href="{{ $d['url_faktur'] }}" target="_blank"
-                                                class="btn btn-primary ps-5 pe-5"
+                                                class="bg-[#3399FE] text-white font-medium py-2 px-5 rounded-lg shadow-md hover:bg-blue-600 transition flex items-center gap-2 w-fit"
                                                 style="border: none;border-radius: 10px;background-color: #8158F4">
                                                 <i class="bi bi-download me-2 "></i>
                                                 Unduh File
@@ -289,9 +289,9 @@
                                         <td colspan="2" class="text-[#475569]">
                                             @if ($d['url_invoice'] == null && $d['url_tanda_terima'] == null)
                                             Invoice belum tersedia
-                                            @elseif ($d['url_tanda_terima'] != null)
+                                            @elseif ($d['url_invoice'] != null)
                                             <a href="{{ route('download.tanda.terima', ['filename' => $d['url_tanda_terima']]) }}"
-                                                class="btn btn-primary ps-5 pe-5"
+                                                class="bg-[#3399FE] text-white font-medium py-2 px-5 rounded-lg shadow-md hover:bg-blue-600 transition flex items-center gap-2 w-fit"
                                                 style="border: none;border-radius: 10px;background-color: #8158F4">
                                                 <i class="bi bi-download me-2 "></i>
                                                 Unduh File
